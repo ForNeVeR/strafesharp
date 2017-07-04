3ca600d93fa97c3078096dff0f5d3726cdbf1d0a
Friedrich von Never <friedrich@fornever.me>
2017-07-04 23:07:08 +0700

    Tests: finish frontend tests

--- StrafeSharp.Tests/FrontendTests.fs
+++ StrafeSharp.Tests/FrontendTests.fs
@@ -25,15 +25,41 @@ let ``Each packet has a length of 60`` () =
     datagram.Packets
     |> Array.iter (fun a -> Assert.Equal(60, a.Length))
 
+let private colorPacketHeader n =
+    let index = byte (n / 4)
+    [| 127uy; index; 60uy; 0uy |]
+
+let private resizeTo60 a = Array.Resize(a, 60); a.Value
+
+let splitValueTest baseIndex colorArray datagram =
+    let expectedPacket0 = Array.concat [| colorPacketHeader 0; Array.take 60 colorArray |]
+    let expectedPacket1 = Array.concat [| colorPacketHeader 1; Array.take 60 colorArray |]
+    let expectedPacket2 =
+        ref (Array.concat [| colorPacketHeader 1; Array.take 60 colorArray |])
+        |> resizeTo60
+    Assert.Equal<byte>(expectedPacket0, datagram.Packets.[baseIndex])
+    Assert.Equal<byte>(expectedPacket1, datagram.Packets.[baseIndex + 1])
+    Assert.Equal<byte>(expectedPacket2, datagram.Packets.[baseIndex + 2])
+
+[<Fact>]
+let ``Color values are distributed between the corresponding packets`` () =
+    let keyboardState =
+        { RedValues = [| 1uy .. byte KeyboardState.KeyCount |]
+          GreenValues = [| byte KeyboardState.KeyCount .. 1uy |]
+          BlueValues = [| byte KeyboardState.KeyCount .. 1uy |] }
+    let datagram = Frontend.Render keyboardState
+    splitValueTest 0 keyboardState.RedValues datagram
+    splitValueTest 4 keyboardState.GreenValues datagram
+    splitValueTest 8 keyboardState.BlueValues datagram
+
 [<Fact>]
 let ``Packets 3, 7 and 11 have fixed content`` () =
     let keyboardState = KeyboardState.Empty
     let datagram = Frontend.Render keyboardState
-    let resize a = Array.Resize(a, 60); a.Value
     let genFixedPacket = function
-        | 3 -> ref [| 7uy; 40uy; 1uy; 3uy; 1uy |] |> resize
-        | 7 -> ref [| 7uy; 40uy; 2uy; 3uy; 1uy |] |> resize
-        | 11 -> ref [| 7uy; 40uy; 3uy; 3uy; 2uy |] |> resize
+        | 3 -> ref [| 7uy; 40uy; 1uy; 3uy; 1uy |] |> resizeTo60
+        | 7 -> ref [| 7uy; 40uy; 2uy; 3uy; 1uy |] |> resizeTo60
+        | 11 -> ref [| 7uy; 40uy; 3uy; 3uy; 2uy |] |> resizeTo60
         | _ -> failwith "Impossible"
     let packet3 = datagram.Packets.[3]
     let packet7 = datagram.Packets.[7]
